524de1dd4730274f437a45ae418e1d70cbf564af
vladimirlbkn <119615319+hawyya@users.noreply.github.com>
2024-01-17 20:20:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+    // let cont = document.querySelector('.container')
+        
+        // for(i = 0; i<5; i++){
+        // create = document.createElement('div')
+        // create.style.width = '500px'
+        // create.style.height='500px'
+        // create.style.marginBottom = '50px'
+        // create.style.background = 'black'
+        // cont.appendChild(create)
+        // }
+
+        // create = document.createElement('button')
+        // createTwo = document.createElement('p')
+        // create.style.width = '90px'
+        // create.style.height = '30px'
+        // create.innerHTML = 'button'
+        // createTwo.textContent = "Clicked: 0 times."
+        // document.querySelector('body').appendChild(create)
+        // document.querySelector('body').appendChild(createTwo)
+
+        // let cont = document.querySelector('.container')
+        // for(i = 1; i<8; i++){
+        //     create = document.createElement('li')
+        //     create.innerHTML = i 
+        //     cont.appendChild(create)
+        // }
+
+
+        // let p = document.querySelector('p')
+        // p.innerHTML = '<b>Hello</b>'
+        // p.textContent = '<b>Hello</b>'
+
+
+
+    
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//сделать 2 input с innerHTML && textContent
